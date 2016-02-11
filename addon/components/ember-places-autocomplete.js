11239426ff865944b59e3dec08a315b9a790b913
Taras Mankovski <tarasm@gmail.com>
2016-02-11 18:08:39 -0500

    Added mechanism

--- addon/components/ember-places-autocomplete.js
+++ addon/components/ember-places-autocomplete.js
@@ -2,7 +2,8 @@ import Ember from 'ember';
 import layout from '../templates/components/ember-places-autocomplete';
 
 const {
-  inject
+  inject,
+  run
 } = Ember;
 
 export default Ember.Component.extend({
@@ -12,11 +13,24 @@ export default Ember.Component.extend({
     this._super(...arguments);
     this.get('placesAutocomplete').on('selected', this, this.notifyContext);
   },
+  willInsertElement() {
+    this._super(...arguments);
+    run.scheduleOnce('afterRender', this, this.setupPlacesAutocomplete);
+  },
+  setupPlacesAutocomplete() {
+    let service = this.get('placesAutocomplete');
+    service.setup(this.$('input')[0]);
+  },
+  willDestroyElement() {
+    this._super(...arguments);
+    let service = this.get('placesAutocomplete');
+    service.teardown();
+  },
   willDestroy() {
     this._super(...arguments);
     this.get('placesAutocomplete').off('selected', this, this.notifyContext);
   },
-  notifyContext(data) {
-    this.sendAction('on-select', data);
+  notifyContext(location, place) {
+    this.sendAction('on-select', location, place);
   }
 });
--- addon/services/places-autocomplete.js
+++ addon/services/places-autocomplete.js
@@ -1,3 +1,4 @@
+/* global google */
 import Ember from 'ember';
 
 const {
@@ -5,7 +6,27 @@ const {
 } = Ember;
 
 export default Ember.Service.extend(Evented, {
-  notify(data) {
-    this.trigger('selected', data);
+  setup(input) {
+    let autocomplete = new google.maps.places.Autocomplete(input);
+    let listener = autocomplete.addListener('place_changed', Ember.run.bind(this, () => {
+		  let place = autocomplete.getPlace();
+		  this.notify({
+        lat: place.geometry.location.lat(),
+        lng: place.geometry.location.lng(),
+        place
+      });
+	 }));
+   this.set('listener', listener);
+   this.set('autocomplete', autocomplete);
+  },
+  teardown() {
+    let listener = this.get('listener');
+    let autocomplete = this.get('autocomplete');
+    
+    google.maps.event.removeListener(listener);
+    google.maps.event.clearInstanceListeners(autocomplete);
+  },
+  notify(place) {
+    this.trigger('selected', place);
   }
 });
